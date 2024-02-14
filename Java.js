70b67ed80ad45dc57259f4d09269b8bb49205ea7
TheRohitsardar <110388039+TheRohitsardar@users.noreply.github.com>
2024-02-14 11:20:24 +0530

    My Portfolio

---
+++ Java.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    SVGStringList: ["Frontend Developer", "Administrator", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
